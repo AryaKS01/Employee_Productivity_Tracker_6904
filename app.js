868c3779d090f8d45c7064392b36b4b338ccf42b
AryaKS01 <aryaworks24@gmail.com>
2025-01-17 16:55:35 +0530

    node

--- app.js
+++ app.js
@@ -18,12 +18,9 @@ app.use("/tasks", taskRoutes);
 app.use("/gamification", gamificationRoutes); // New route for gamification
 
 mongoose
-  .connect("mongodb://localhost:27017/employee-productivity-tracker", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect("mongodb://localhost:27017/employee-productivity-tracker")
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
